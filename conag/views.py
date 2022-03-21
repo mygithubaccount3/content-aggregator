d015e28047a88abe9c12069b36c5d600ae7e49f8
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-03-21 03:03:12 +0200

    configure selenium for heroku

--- conag/views.py
+++ conag/views.py
@@ -62,10 +62,10 @@ def cnn(request):
     articles = []
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-    chrome_options.add_argument('headless')
-    chrome_options.add_argument('disable-gpu')
-    chrome_options.add_argument('no-sandbox')
-    chrome_options.add_argument('disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
 
     browser = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
 
