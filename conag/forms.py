38b2d3503eb45c4fa0c168219a860f0b71a018a5
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2022-03-25 01:56:32 +0200

    remove search query requirements

--- conag/forms.py
+++ conag/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label='', min_length=1,
-                            max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control me-2', 'type': 'search', 'placeholder': 'Search', 'aria-label': 'Search', 'style': 'width: 44%'}))
+    query = forms.CharField(label='',
+                            max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control me-2', 'type': 'search', 'placeholder': 'Search', 'aria-label': 'Search', 'style': 'width: 44%'}))
